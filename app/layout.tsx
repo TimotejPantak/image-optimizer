4020f0f9449ca58a54cbd561f96877d88d45613b
TimotejPantak <info@timotejpantak.com>
2026-02-15 21:23:19 +0100

    Add speedinsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 export const metadata: Metadata = {
   title: 'Free Image Converter | Convert PNG, JPG to WebP & AVIF',
@@ -35,6 +37,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
